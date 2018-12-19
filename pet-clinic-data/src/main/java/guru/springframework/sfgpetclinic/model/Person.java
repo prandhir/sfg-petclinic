e0fc378e248345549804e73120856adcb4365d9c
Pankaj <pankaj.randhir@gmail.com>
2018-12-18 21:28:26 -0500

    adding base entity and correcting model classes

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -12,11 +12,11 @@ import javax.persistence.Column;
 @MappedSuperclass*/
 public class Person extends BaseEntity {
 
-    public Person(Long id, String firstName, String lastName) {
+  /*  public Person(Long id, String firstName, String lastName) {
         //super(id);
         this.firstName = firstName;
         this.lastName = lastName;
-    }
+    }*/
 
     @Column(name = "first_name")
     private String firstName;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Builder
 @Entity
 @Table(name = "vets")*/
-public class Vet /*extends Person*/ {
+public class Vet extends Person {
 
     /*@ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
